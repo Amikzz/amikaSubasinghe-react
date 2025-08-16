1ffacf86dedbfe6e786222ee2b15d121bed6ab7c
Amika Subasinghe <amixx2005@gmail.com>
2025-08-16 17:18:40 +0530

    feat: add Code component with typing animation and user input functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Content from "./components/Content";
 import Loader from "./components/Loader";
 import Techstack from "./components/Techstack";
+import Code from "./components/Code";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -21,6 +22,7 @@ const App = () => {
           <Header />
           <main className="flex flex-col items-center justify-center bg-zinc-900 text-zinc-50">
             <Hero />
+            <Code />
             <Content />
             <Techstack />
           </main>
